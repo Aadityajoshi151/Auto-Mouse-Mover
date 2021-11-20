3a37f007ee2e0e3729251f46530034afcfa6c5c4
Aaditya Joshi <aadityajoshi151@gmail.com>
2021-11-20 10:34:36 +0530

    Added schedule method

--- master.py
+++ master.py
@@ -1,6 +1,7 @@
 from screeninfo import get_monitors
 import autopy
 import random
+import schedule
 
 monitors_list = get_monitors()
 width = getattr(monitors_list[0],'width') 
@@ -8,3 +9,5 @@ height = getattr(monitors_list[0],'height')
 
 def moveMouseCursor():
     autopy.mouse.smooth_move(random.randint(0,width), random.randint(0,height))
+
+schedule.every(10).seconds.do(moveMouseCursor)
