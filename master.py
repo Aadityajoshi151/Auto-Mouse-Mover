cd55903de9348fbae6d99d5f628e29d11982e225
Aaditya Joshi <aadityajoshi151@gmail.com>
2021-11-20 10:30:24 +0530

    added mouse mover function
    This function moves mouse cursor smoothly to a random point on the screen

--- master.py
+++ master.py
@@ -1,5 +1,10 @@
 from screeninfo import get_monitors
+import autopy
+import random
 
 monitors_list = get_monitors()
 width = getattr(monitors_list[0],'width') 
-height = getattr(monitors_list[0],'height') 
+height = getattr(monitors_list[0],'height')
+
+def moveMouseCursor():
+    autopy.mouse.smooth_move(random.randint(0,width), random.randint(0,height))
