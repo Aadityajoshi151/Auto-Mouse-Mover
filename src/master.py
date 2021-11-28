fd14255cc5cfefdc33ba88b5b37531b022490587
Aaditya Joshi <aadityajoshi151@gmail.com>
2021-11-28 11:14:46 +0530

    added temp arg parsers

--- src/master.py
+++ src/master.py
@@ -6,20 +6,35 @@ import time
 import argparse
 
 parser = argparse.ArgumentParser(description= "Small and easy python script to move the mouse cursor and stop the system from locking. Made for individuals working in the IT field who use the laptop provided by their employer and have no control over screen lock time.")
+parser.add_argument("-n", type=int, help="Number of Hours/minutes/seconds")
 
-monitors_list = get_monitors()
-width = getattr(monitors_list[0],'width') 
-height = getattr(monitors_list[0],'height')
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-s',action='store_const', dest='type', const='s')
+group.add_argument('-m',action='store_const', dest='type', const='m')
+group.add_argument('-H',action='store_const', dest='type', const='H')
 
-def moveMouseCursor():
-    autopy.mouse.smooth_move(random.randint(0,width), random.randint(0,height))
+args = parser.parse_args()
 
-print("Auto Mouse Mover Running")
-print("Press Ctrl+C To Stop")
+# monitors_list = get_monitors()
+# width = getattr(monitors_list[0],'width') 
+# height = getattr(monitors_list[0],'height')
 
-schedule.every(2).minutes.do(moveMouseCursor)
+# def moveMouseCursor():
+#     autopy.mouse.smooth_move(random.randint(0,width), random.randint(0,height))
+
+# print("Auto Mouse Mover Running")
+# print("Press Ctrl+C To Stop")
+
+# schedule.every(2).minutes.do(moveMouseCursor)
 
 if __name__ == "__main__":
-    while(True):
-        schedule.run_pending()
-        time.sleep(1)
+    # while(True):
+    #     schedule.run_pending()
+    #     time.sleep(1)
+    print(args.n)
+    if args.type == 's':
+        print(f"{args.n} seconds")
+    elif args.type == "m":
+        print(f"{args.n} minutes")
+    else:
+        print(f"{args.n} Hours")
